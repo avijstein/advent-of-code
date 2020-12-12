177ca058616da7391fb9897e729e90cff2d10ede
avijstein <ajstein@wesleyan.edu>
2020-12-12 14:12:11 -0500

    solution to day1

--- 2020/day1.py
+++ 2020/day1.py
@@ -4,7 +4,12 @@ os.chdir('../github/advent')
 
 nums = [1721,979,366,299,675,1456]
 
-def advent1(nums):
+
+def advent1a(nums):
+    """
+    given a list of ints, loop through the list to find every pair
+    Add them together, and see if any equal 2020.
+    """
     for i in range(0,len(nums)):
         for j in range(i,len(nums)):
             if nums[i] + nums[j] == 2020:
@@ -13,23 +18,23 @@ def advent1(nums):
 
 
 def advent1b(nums):
+    """
+    given a list of ints, do the same as advent1a, but for three numbers.
+    """
     for i in range(0,len(nums)):
-        for j in range(0,len(nums)):
-            for k in range(0,len(nums)):
+        for j in range(i,len(nums)):
+            for k in range(j,len(nums)):
                 if (nums[i] + nums[j] + nums[k]) == 2020:
                     return nums[i], nums[j], nums[k], nums[i]*nums[j]*nums[k]
-                print(nums[i], nums[j], nums[k])
-        return 'no such expenses'
+    return 'no such expenses'
 
 
 
 with open('2020/day1.txt') as f:
     new_lines = [int(x.replace('\n', '')) for x in f.readlines()]
-    for i in range(0,10):
-        print(i, new_lines[i])
-    # print(advent1b(new_lines))
+    print(advent1a(new_lines))
+    print(advent1b(new_lines))
 
-# print(advent1(nums))
 
 
 
