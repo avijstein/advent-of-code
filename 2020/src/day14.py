4c2c0f298e62a9821368fcef2b138a55e5128989
avijstein <avijstein@gmail.com>
2021-04-05 21:14:45 -0400

    refactor v1 for the second part

--- 2020/src/day14.py
+++ 2020/src/day14.py
@@ -3,15 +3,14 @@ import os
 os.chdir('../github/advent/2020')
 
 
-# data = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-# mem[8] = 11
-# mem[7] = 101
-# mem[8] = 0"""
+data = """mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"""
 
 # print(data)
 
-
-def recode_num(num):
+def recode_num(mask, num):
     bin_num = bin(int(num))[2:].zfill(36)
     new_num = []
     for i in range(len(mask)):
@@ -23,35 +22,61 @@ def recode_num(num):
     return new_num
 
 
+def address_mask(mask, address):
+    xs = mask.count('X')
+    adds = []
+    for i in range(2**xs):
+        fill = bin(i)[2:].zfill(xs)
+        new_mask, counter = list(mask), 0
+        for j in range(len(new_mask)):
+            if new_mask[j] == 'X':
+                new_mask[j] = fill[counter]
+                counter += 1
+        new_mask = ''.join(new_mask)
+        new_add = recode_num(new_mask, address)
+        adds.append(new_add)
+    return adds
 
-mask = '0' * 36
-mem = dict()
 
-with open('./data/day14.txt') as f:
-    lines = f.read().split('\n')[:-1]
+
+mask = "000000000000000000000000000000X1001X"
+address = 42
+
+print(address_mask(mask, address))
+
+
+
+
+def v1(lines):
+    mask = '0' * 36
+    mem = dict()
     for line in lines:
-        print(line)
         if "mask = " in line:
             mask = line.replace('mask = ', '')
         else:
             address, num = line[4:].split('] = ')
-            mem[address] = recode_num(num)
+            mem[address] = recode_num(mask, num)
+    total = sum(mem.values())
+    return total
 
-print('total:', sum(mem.values()))
+def v2(lines):
+    mask = '0' * 36
+    mem = dict()
+    for line in lines:
+        if "mask = " in line:
+            mask = line.replace('mask = ', '')
+        else:
+            address, num = line[4:].split('] = ')
+            mem[address] = recode_num(mask, num)
+    total = sum(mem.values())
+    return total
 
 
+with open('./data/day14.txt') as f:
+    lines = f.read().split('\n')[:-1]
+    # print(v2(lines))
 
 
-# for line in data.split('\n'):
-#     if "mask = " in line:
-#         mask = line.replace('mask = ', '')
-#         # print('new mask:', mask)
-#     else:
-#         address, num = line[4:].split('] = ')
-#         mem[address] = recode_num(num)
-
-# print(mem.values())
-# print('total:', sum(mem.values()))
 
 
 # fin
